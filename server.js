d2f668d716f9e223bc20522869f49425ffd0040d
Nagaraj <raj.nagaraj1990@gmail.com>
2017-05-14 17:37:30 +0530

    s

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var aliveuser = null;
 var clients = {};
 var savedhash = null;
 
-server.listen(process.env.port || 3000);
+server.listen(process.env.PORT || 3000);
 
 console.log('server running');
 
